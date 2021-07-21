98758f64898add88783cc29ada81ddd2786f636c
hyoogu <idhyo0o@gmail.com>
2021-07-21 13:46:09 +0900

    Add pipeline setting code

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/gallery-port/server
 ls -al
-authbind --deep pm2 start ./index.js
+authbind --deep pm2 start /home/ubuntu/gallery-port/server/index.js
 # node index.js &
