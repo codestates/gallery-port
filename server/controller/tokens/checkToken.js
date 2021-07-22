b2844cd796b837e97f542ab35b65c8d1a57f1b9c
hyoogu <idhyo0o@gmail.com>
2021-07-22 19:30:19 +0900

    Simplify checkToken middleware

--- server/controller/tokens/checkToken.js
+++ server/controller/tokens/checkToken.js
@@ -5,48 +5,48 @@ const {
 	sendRefreshToken,
 	verifyAccessToken,
 	verifyRefreshToken,
+	getDataValues
 } = require('./tokenFunctions');
-
-// case1: access token과 refresh token 모두가 만료된 경우 -> 에러 발생
-// case2: access token은 만료됐지만, refresh token은 유효한 경우 -> access token 재발급
-// case3: access token은 유효하지만, refresh token은 만료된 경우 -> refresh token 재발급
-// case4: access token과 refresh token 모두가 유효한 경우 -> 다음 미들웨어로
 
 module.exports = {
 	checkToken: async (req, res, next) => {
 		const accessTokenData = verifyAccessToken(req);
 		const refreshTokenData = verifyRefreshToken(req);
-		console.log(accessTokenData);
-		console.log(refreshTokenData);
-		console.log('-----------');
-		if (accessTokenData === null) {
-			if (refreshTokenData === null) {	// case1
-				res.status(404).send({ message: 'Not found' });
-			} else {	// case2
-				const { user_email } = refreshTokenData;
-				const data = await User.findOne({
-					where: {
-						user_email
-					}
-				});
-				const newAccessToken = generateAccessToken(data.dataValues);
-				return newAccessToken;
-			}
-		} else {
-			if (refreshTokenData === null) { // case3
-				const { user_email } = accessTokenData;
-				const data = await User.findOne({
-					where: {
-						user_email
-					}
-				});
-				console.log(data);
-				const newRefreshToken = generateRefreshToken(data.dataValues);
-				sendRefreshToken(res, newRefreshToken);
-				next();
-			} else { // case4
-				next();
-			}
+
+		// case1: access token과 refresh token 모두가 만료된 경우 -> 에러 발생
+		if (accessTokenData === null && refreshTokenData === null) {
+				res.status(401).send({ message: 'Not found' });
 		}
+		// case2: access token은 만료됐지만, refresh token은 유효한 경우 -> access token 재발급
+		if (accessTokenData === null && refreshTokenData !== null) {
+			const { user_email } = refreshTokenData;
+			const data = await User.findOne({
+				where: {
+					user_email
+				}
+			});
+			const dataValues = getDataValues(data);
+			const newAccessToken = generateAccessToken(dataValues);
+			req.headers.authorization = `Bearer ${newAccessToken}`;
+			next();
+		}
+		// case3: access token은 유효하지만, refresh token은 만료된 경우 -> refresh token 재발급
+		if (accessTokenData !== null && refreshTokenData === null) {
+			const { user_email } = accessTokenData;
+			const data = await User.findOne({
+				where: {
+					user_email
+				}
+			});
+			const dataValues = getDataValues(data);
+			const newRefreshToken = generateRefreshToken(dataValues);
+			sendRefreshToken(res, newRefreshToken);
+			next();
+		}
+		// case4: access token과 refresh token 모두가 유효한 경우 -> 다음 미들웨어로
+		if (accessTokenData !== null && refreshTokenData !== null) {
+			next();
+		}
+
 	}
 }
