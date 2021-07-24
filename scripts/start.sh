5a6442461c4f1589cd253214e69e3ec9b687be18
hyoogu <idhyo0o@gmail.com>
2021-07-24 16:17:45 +0900

    Add deploy setting

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,5 +5,7 @@ export DB_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DB_P
 export DB_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DB_PORT --query Parameters[0].Value | sed 's/"//g')
 export DB_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DB_HOST --query Parameters[0].Value | sed 's/"//g')
 export NODE_ENV=$(aws ssm get-parameters --region ap-northeast-2 --names NODE_ENV --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
