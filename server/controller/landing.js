79cf42e744161e7224ae318465ebb2137853f037
hyoogu <idhyo0o@gmail.com>
2021-07-26 03:36:02 +0900

    Add get all project

--- server/controller/landing.js
+++ server/controller/landing.js
@@ -1,31 +1,44 @@
-const { User, Project, StackForProject } = require('../models/index')
+const { User, Project, Stack ,StackForProject } = require('../models/index')
 
 module.exports = {
 
     getAllProjects: async (req, res) => {
+        const stack = req.query.stack;
 
-        // TODO: req.query.stack 가 있으면 stack 별 project 를 조회한다.
-        // TODO: query 가 없다면 전체 project 중 최신순으로 18개를 조회한다. 
+        // 특정 스택 조회
+        if (stack) {
+            // stack id 조회
+            const stackId = await Stack.findOne({
+                where: {
+                    stack_name: stack
+                }
+            }).then(stack => {
+                return stack.id;
+            }).catch(err => {
+                return res.status(404).send({ "message": "Not Found" });
+            });
 
-        // TODO: status 200 
-        // { "data" : 
-        // 		[
-        // 		...
-        // 		    {
-        // 						"id" : id, 
-        // 		        "project_thumbnail": path,
-        // 		        "project_name": "example"
-        // 		    }
-        // 		],
-        // 	"message" : "ok"
-        // } 
+            // 스택에 해당하는 프로젝트 id
+            const projectsId = await StackForProject.findAll({
+                where: {
+                    stack_id: stackId
+                },
+                attributes: ['project_id']
+            }).then(data => data.map(project => (project.project_id)));
 
-        // TODO: status 404
-        // {
-        //     "message": "Not Found"
-        // }
-        
-        res.json({'landing' : 'page'})
+            // 해당 프로젝트 id 조회
+            const projects = await Project.findAll({
+                where: {
+                    id: projectsId
+                },
+                attributes: ['id','project_thumbnail', 'project_name']
+            });
+            
+            res.status(200).json({ "data": projects, "message": "Ok" });
+        } else {
+            const projects = await Project.findAll();
+            res.status(200).json({ "data": projects, "message": "Ok" });
+        }
     },
-
+    
 };
