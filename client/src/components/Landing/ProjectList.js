1ab599b828eced6052adf1b73ece8138eccbd03d
jong-ah <jonga6431@gmail.com>
2021-07-25 17:23:04 +0900

    Add profile page

--- client/src/components/Landing/ProjectList.js
+++ client/src/components/Landing/ProjectList.js
@@ -6,8 +6,23 @@ function ProjectList(project) {
     window.location.href = './project';
   }
 
-  return (
-    <div className="list" key={project.project.id}>
+  function handleUpload(e) {
+    window.location.href = './upload';
+  }
+  console.log(project.project.project_new);
+
+  return project.project.project_new ? (
+    <div className="list" onClick={() => handleUpload()}>
+      <div onClick={handleClick}>
+        <img
+          src={project.project.project_thumbnail}
+          alt={project.project.project_name}
+        ></img>
+        <p className="listName">{project.project.project_name}</p>
+      </div>
+    </div>
+  ) : (
+    <div className="list">
       <div onClick={handleClick}>
         <img
           src={project.project.project_thumbnail}
