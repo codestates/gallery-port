b9c9236657bdf2e7ba8cb22c4713809df58f41b6
hyoogu <idhyo0o@gmail.com>
2021-07-26 14:01:13 +0900

    Add deploy setting

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,7 @@ export NODE_ENV=$(aws ssm get-parameters --region ap-northeast-2 --names NODE_EN
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
 export IMAGE_ENDPOINT=$(aws ssm get-parameters --region ap-northeast-2 --names IMAGE_ENDPOINT --query Parameters[0].Value | sed 's/"//g')
+export CLIENT_ENDPOINT=$(aws ssm get-parameters --region ap-northeast-2 --names CLIENT_ENDPOINT --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
 npx sequelize-cli db:seed:all
