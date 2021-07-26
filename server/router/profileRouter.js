8c48a2832c17400e825cbbc110b566f6a162712c
hyoogu <idhyo0o@gmail.com>
2021-07-26 13:53:26 +0900

    Remove checktoken middleware

--- server/router/profileRouter.js
+++ server/router/profileRouter.js
@@ -1,8 +1,7 @@
 const { getProfileData } = require('../controller/profile');
-const { checkToken } = require('../controller/tokens/checkToken');
 const express = require('express');
 const router = express.Router();
 
-router.get('/:id', checkToken, getProfileData);
+router.get('/:id', getProfileData);
 
 module.exports = router;
